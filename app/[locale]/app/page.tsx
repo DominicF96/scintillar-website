0ad2024dc7644e64ce89176a12e356557e94a012
unknown <me@dominicfournier.com>
2025-01-04 01:42:09 -0500

    Add message

--- app/[locale]/app/page.tsx
+++ app/[locale]/app/page.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-// import { Locale } from "@/i18n.config";
+import { Locale } from "@/i18n.config";
 import { getUser } from "@/app/api/auth/auth";
+import * as i18n from "@/i18n/app/dashboard.i18n";
 
-// type Props = {
-//   params: {
-//     locale: Locale;
-//   };
-// };
+type Props = {
+  params: {
+    locale: Locale;
+  };
+};
 
-async function Page() {
-  // const { locale } = await params;
+async function Page({ params }: Props) {
+  const { locale } = await params;
+  const t = i18n[locale];
   const user = await getUser();
   return (
     <div className="max-w-[1200px] mx-auto p-4">
+      <h2>{t.title}</h2>
+      <p>{t.description}</p>
       <pre className="my-2 text-foreground p-8 overflow-x-scroll bg-card rounded-lg">
         <code>{JSON.stringify(user, null, 2)}</code>
       </pre>
---
+++ i18n/app/dashboard.i18n.ts
@@ -0,0 +1,16 @@
+export interface I18n {
+  title: string;
+  description: string;
+}
+
+export const en: I18n = {
+  title: "Dashboard",
+  description:
+    "No features are available at the moment, please check back later.",
+};
+
+export const fr: I18n = {
+  title: "Tableau de bord",
+  description:
+    "Aucune fonctionnalité n'est disponible pour le moment, veuillez revenir plus tard.",
+};
